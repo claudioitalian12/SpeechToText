fd4d46891647997e36e73d2637bef0eabc43ed44
claudioitalian12 <dark.claudio12@gmail.com>
2019-01-17 22:22:33 +0100

    Initial Commit

---
+++ SpeechToText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechToText
+//
+//  Created by claudio Cavalli on 17/01/2019.
+//  Copyright © 2019 claudio Cavalli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
